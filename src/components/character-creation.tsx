2f078d68aacab70823513116b28829d6e9c3f4ae
Alan Yang <alanyang411@gmail.com>
2025-03-12 18:36:28 -0400

    Add stats to character creation (#1)
    * feat: add tabs
    
    * feat: add stat points table
    
    * feat: randomize stats
    
    * feat: reroll info and stats separately

--- src/components/character-creation.tsx
+++ src/components/character-creation.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { format } from 'date-fns'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { RandomAvatar } from 'react-random-avatar'
+import type { Entries } from 'type-fest'
 import { useIsMounted } from 'usehooks-ts'
 
 import { useCharacterStats } from '~/hooks/use-character-stats'
 import { useGame } from '~/hooks/use-game'
+import { capitalize, randomInt } from '~/lib/utils'
 
 import { Button } from './ui/button'
 import {
@@ -20,11 +22,31 @@ import {
 import { DatePicker } from './ui/date-picker'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './ui/table'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
+
+const statEmojis = {
+  agility: '🏃',
+  charisma: '✨',
+  intelligence: '🧠',
+  strength: '💪',
+  wisdom: '🔮',
+}
 
 export function CharacterCreation() {
-  const { avatar, name, birthday, updateCharacterStats } = useCharacterStats()
+  const { info, stats, updateCharacterStats } = useCharacterStats()
   const { initialized, resetGame, updateGame } = useGame()
   const isMounted = useIsMounted()()
+  const [activeTab, setActiveTab] = useState('basic-info')
 
   useEffect(() => {
     if (!initialized) {
@@ -39,7 +61,7 @@ export function CharacterCreation() {
 
   const startLife = () => {
     window.alert(
-      `Congratulations! You were born on ${format(birthday, 'MMMM d')}. Your parents have named you ${name}.`
+      `Congratulations! You were born on ${format(info.birthday, 'MMMM d')}. Your parents have named you ${info.name}.`
     )
   }
 
@@ -50,51 +72,119 @@ export function CharacterCreation() {
         <CardDescription>Create your character</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className='relative mb-4 flex items-center justify-center'>
-          <RandomAvatar seed={avatar} size={15} />
-        </div>
-        <form autoComplete='off'>
-          <div className='grid w-full items-center gap-4'>
-            <div className='flex flex-col space-y-1.5'>
-              <Label htmlFor='name'>Name</Label>
-              <Input
-                id='name'
-                placeholder=''
-                type='text'
-                value={name}
-                onChange={(e) => {
-                  updateCharacterStats('name', e.target.value)
-                }}
-              />
+        <Tabs
+          defaultValue='basic-info'
+          value={activeTab}
+          onValueChange={setActiveTab}
+        >
+          <TabsList className='grid w-full grid-cols-2'>
+            <TabsTrigger value='basic-info'>Basic Info</TabsTrigger>
+            <TabsTrigger value='stat-points'>Stat Points</TabsTrigger>
+          </TabsList>
+
+          <TabsContent className='mt-4 space-y-4' value='basic-info'>
+            <div className='relative flex items-center justify-center'>
+              <RandomAvatar seed={info.avatar} size={15} />
             </div>
-            <div className='flex flex-col space-y-1.5'>
-              <Label htmlFor='birthday'>Birthday</Label>
-              <DatePicker
-                id='birthday'
-                selected={birthday}
-                calendarProps={{
-                  formatters: {
-                    formatCaption: (date) => format(date, 'MMMM'),
-                  },
-                }}
-                onSelect={(date) => {
-                  updateCharacterStats('birthday', date)
-                }}
-              />
+            <div className='grid w-full items-center gap-4'>
+              <div className='flex flex-col space-y-1.5'>
+                <Label htmlFor='name'>Name</Label>
+                <Input
+                  id='name'
+                  placeholder=''
+                  type='text'
+                  value={info.name}
+                  onChange={(e) => {
+                    updateCharacterStats('name', e.target.value)
+                  }}
+                />
+              </div>
+              <div className='flex flex-col space-y-1.5'>
+                <Label htmlFor='birthday'>Birthday</Label>
+                <DatePicker
+                  id='birthday'
+                  selected={info.birthday}
+                  calendarProps={{
+                    formatters: {
+                      formatCaption: (date) => format(date, 'MMMM'),
+                    },
+                  }}
+                  onSelect={(date) => {
+                    updateCharacterStats('birthday', date)
+                  }}
+                />
+              </div>
             </div>
-          </div>
-        </form>
+            <div className='flex'>
+              <Button
+                className='flex flex-1'
+                variant='secondary'
+                onClick={() => {
+                  resetGame()
+                }}
+              >
+                🎲 Reroll Info
+              </Button>
+            </div>
+          </TabsContent>
+
+          <TabsContent className='mt-4 space-y-4' value='stat-points'>
+            <Table>
+              <TableCaption>
+                You start a life with between{' '}
+                <span className='text-foreground font-medium'>0</span> and{' '}
+                <span className='text-foreground font-medium'>15</span> stat
+                points.
+              </TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Stat</TableHead>
+                  <TableHead className='text-right'>Points</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {(Object.entries(stats) as Entries<typeof stats>).map(
+                  ([stat, value]) => (
+                    <TableRow key={stat}>
+                      <TableCell>
+                        {statEmojis[stat]} {capitalize(stat)}
+                      </TableCell>
+                      <TableCell className='text-right'>{value}</TableCell>
+                    </TableRow>
+                  )
+                )}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={1}>Total</TableCell>
+                  <TableCell className='text-right'>
+                    {Object.values(stats).reduce(
+                      (acc, value) => acc + value,
+                      0
+                    )}
+                  </TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+            <div className='flex'>
+              <Button
+                className='flex flex-1'
+                variant='secondary'
+                onClick={() => {
+                  ;(Object.entries(stats) as Entries<typeof stats>).forEach(
+                    ([stat]) => {
+                      updateCharacterStats(stat, randomInt(0, 3))
+                    }
+                  )
+                }}
+              >
+                🎲 Reroll Stat Points
+              </Button>
+            </div>
+          </TabsContent>
+        </Tabs>
       </CardContent>
       <CardFooter className='flex gap-4'>
-        <Button
-          className='flex flex-1'
-          variant='secondary'
-          onClick={() => {
-            resetGame()
-          }}
-        >
-          🎲 Reroll
-        </Button>
         <Button className='flex flex-1' onClick={startLife}>
           Start Life
         </Button>
--- src/hooks/use-character-stats.ts
+++ src/hooks/use-character-stats.ts
@@ -28,11 +28,11 @@ export function useCharacterStats() {
       age: 0,
       gender: '',
       profession: '',
-      agility: 0,
-      charisma: 0,
-      intelligence: 0,
-      strength: 0,
-      wisdom: 0,
+      agility: randomInt(0, 3),
+      charisma: randomInt(0, 3),
+      intelligence: randomInt(0, 3),
+      strength: randomInt(0, 3),
+      wisdom: randomInt(0, 3),
     })
   }
 
@@ -43,8 +43,26 @@ export function useCharacterStats() {
     setCharacterStats((prev) => ({ ...prev, [key]: value }))
   }
 
+  const info = {
+    avatar: characterStats.avatar,
+    name: characterStats.name,
+    birthday: characterStats.birthday,
+    age: characterStats.age,
+    gender: characterStats.gender,
+    profession: characterStats.profession,
+  }
+
+  const stats = {
+    agility: characterStats.agility,
+    charisma: characterStats.charisma,
+    intelligence: characterStats.intelligence,
+    strength: characterStats.strength,
+    wisdom: characterStats.wisdom,
+  }
+
   return {
-    ...characterStats,
+    info,
+    stats,
     resetCharacterStats,
     updateCharacterStats,
   }
