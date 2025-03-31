37b116380735101ad7cca7c351b8dddaae36a950
Alan Yang <me@alanyang.com>
2025-03-30 18:17:34 -0700

    feat: add game save functions

--- src/hooks/use-game.ts
+++ src/hooks/use-game.ts
@@ -1,8 +1,18 @@
 import { useLocalStorage } from 'usehooks-ts'
 
 import type { Game } from '~/schemas/game'
+import { gameSaveSchema } from '~/schemas/game'
+
+import { useCharacterAssets } from './use-character-assets'
+import { useCharacterInfo } from './use-character-info'
+import { useCharacterStats } from './use-character-stats'
+import { useCharacterStatus } from './use-character-status'
 
 export function useGame() {
+  const { characterAssets, setCharacterAssets } = useCharacterAssets()
+  const { characterInfo, setCharacterInfo } = useCharacterInfo()
+  const { characterStats, setCharacterStats } = useCharacterStats()
+  const { characterStatus, setCharacterStatus } = useCharacterStatus()
   const [game, setGame] = useLocalStorage<Game>('game', {
     initialized: false,
     started: false,
@@ -12,9 +22,43 @@ export function useGame() {
     setGame((prev) => ({ ...prev, [key]: value }))
   }
 
+  const exportGameSave = () => {
+    try {
+      const saveData = gameSaveSchema.parse({
+        characterAssets,
+        characterInfo,
+        characterStats,
+        characterStatus,
+      })
+
+      return btoa(JSON.stringify(saveData))
+    } catch (_error) {
+      return null
+    }
+  }
+
+  const loadGameSave = (gameSave: string) => {
+    try {
+      const saveData = gameSaveSchema.parse(JSON.parse(atob(gameSave)))
+
+      setCharacterAssets(saveData.characterAssets)
+      setCharacterInfo(saveData.characterInfo)
+      setCharacterStats(saveData.characterStats)
+      setCharacterStatus(saveData.characterStatus)
+      setGame({
+        initialized: true,
+        started: true,
+      })
+    } catch (_error) {
+      return null
+    }
+  }
+
   return {
     game,
     setGame,
     updateGame,
+    exportGameSave,
+    loadGameSave,
   }
 }
--- src/schemas/game.ts
+++ src/schemas/game.ts
@@ -1,12 +1,25 @@
 import { z } from 'zod'
 
+import { characterAssetsSchema } from './character-assets'
+import { characterInfoSchema } from './character-info'
+import { characterStatsSchema } from './character-stats'
+import { characterStatusSchema } from './character-status'
+
 const gameSchema = z.object({
   initialized: z.boolean(),
   started: z.boolean(),
 })
 
-export { gameSchema }
+const gameSaveSchema = z.object({
+  characterAssets: characterAssetsSchema,
+  characterInfo: characterInfoSchema,
+  characterStats: characterStatsSchema,
+  characterStatus: characterStatusSchema,
+})
+
+export { gameSaveSchema, gameSchema }
 
 type Game = z.infer<typeof gameSchema>
+type GameSave = z.infer<typeof gameSaveSchema>
 
-export type { Game }
+export type { Game, GameSave }
